120313304f2f0d0ec7a560c06fdfea09f59335b8
capsNICK <alexandervmalysh@gmail.com>
2022-10-05 07:52:04 +0500

    Initial commit.

---
+++ src/ch01/sharpen/your/pencil/ch01_p43.java
@@ -0,0 +1,15 @@
+package ch01.sharpen.your.pencil;
+
+public class ch01_p43 {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x < 3) {
+            System.out.print("Doo");
+            System.out.print("Bee");
+            x = x + 1;
+        }
+        if (x == 3) {
+            System.out.print("Do");
+        }
+    }
+}
